a7002164a59e7914ff0cb2e1778680c0066ce741
Tomi Lotila <56802355+lotila@users.noreply.github.com>
2022-02-27 02:28:47 +0200

    Update accel_stepper_testi.ino
    code cleanup

--- accel_stepper_testi.ino
+++ accel_stepper_testi.ino
@@ -5,37 +5,28 @@
 // Include the Arduino Stepper.h library:
 #include <Stepper.h>
 const int buttonPin = 7;     // the number of the pushbutton pin
-//const int ledPin =  13;      // the number of the LED pin
 // Define number of steps per rotation:
 const int stepsPerRevolution = 2048;
-const int RevolutionsPerMove = 100;//228;
+const int RevolutionsPerMove = 228;
 int counter = 0;  
-    bool directionUP = true;  
+bool directionUP = true;  
     
 bool morning = true;
 bool stopped = true;
 int buttonState = LOW;         // variable for reading the pushbutton status
 unsigned long previousDayMillis = 0;
-const unsigned long dayTIme =21600000;//43200000;//1000*60*60*12;
+const unsigned long dayTIme =43200000;//1000*60*60*12;
 int motorSpeed = 15;
 
-// Wiring:
-// Pin 8 to IN1 on the ULN2003 driver
-// Pin 9 to IN2 on the ULN2003 driver
-// Pin 10 to IN3 on the ULN2003 driver
-// Pin 11 to IN4 on the ULN2003 driver
 // Create stepper object called 'myStepper', note the pin order:
 Stepper myStepper = Stepper(stepsPerRevolution, 9,11,10,12);
-
 
 void setup() {
   
   // Begin Serial communication at a baud rate of 9600:
-  
   pinMode(buttonPin, INPUT);
   myStepper.setSpeed(motorSpeed);
   Serial.begin(9600);
- 
 }
 void loop() {
 buttonState = digitalRead(buttonPin);
@@ -45,7 +36,6 @@ unsigned long currentMillis = millis();
   {
     if (buttonState == HIGH)
     {
-      
       startMotor();
       runMotor();
     }
@@ -122,9 +112,14 @@ void runMotor()
 void stopMotor()
 {
     directionUP = !directionUP;
-  stopped = true;
-      digitalWrite(9, LOW);
+    stopped = true;
+  
+  // Motor powers off
+    digitalWrite(9, LOW);
     digitalWrite(10, LOW);
     digitalWrite(11, LOW);
     digitalWrite(12, LOW);
 }
+
+
+
